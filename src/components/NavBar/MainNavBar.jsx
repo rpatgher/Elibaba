4cf4a073783990189398ef8d7e0b16ebb179bcd7
Joseph Shakalo paz <Josephshakalopaz@joseph-shakalo.local>
2024-03-28 17:42:32 -0600

    aguregue boton de add y pestaña de carrito

--- src/components/NavBar/MainNavBar.jsx
+++ src/components/NavBar/MainNavBar.jsx
@@ -1,18 +1,29 @@
+import { useState } from 'react';
 import './MainNavBar.css';
 
 const MainNavBar = () => {
+    const [cartOpen, setCartOpen] = useState(false);
+
     return (
-        <div className="navbar">
-            <h1 className="logo-name">Elibaba</h1>
-            <div className="logo-image">
-                <img src="./img/Elibabalogo.png" alt="" />
+        <>
+            <div className="navbar">
+                <h1 className="logo-name">Elibaba</h1>
+                <div className="logo-image">
+                    <img src="./img/Elibabalogo.png" alt="Logo Elibaba" />
+                </div>
+                <div className="actions">
+                    <i className="fa-solid fa-cart-shopping" onClick={() => setCartOpen(!cartOpen)}></i>
+                    <i className="fa-solid fa-user"></i>
+                </div>
+                <div className={`cart-panel ${cartOpen ? 'open' : ''}`}>
+                    <button className="close-btn" onClick={() => setCartOpen(false)}>X</button>
+                    {/* Aquí iría el contenido de tu carrito de compras */}
+                    <p>Carrito de compras...</p>
+                </div>
             </div>
-            <div className="actions">
-                <i className="fa-solid fa-cart-shopping"></i>
-                <i className="fa-solid fa-user"></i>
-            </div>
-        </div>
-    )
+            {cartOpen && <div className="backdrop" onClick={() => setCartOpen(false)}></div>}
+        </>
+    );
 }
 
-export default MainNavBar
+export default MainNavBar;
